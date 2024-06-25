2b9dc7bd955fb352e52e412bc4489fe0d2a063f9
Mykael Mello <eng.mykael@gmail.com>
2024-06-25 16:03:53 -0300

    initial

---
+++ src/A0009-tipo-enum/A0008-tipo-enum.ts
@@ -0,0 +1,19 @@
+enum Cores {
+  VERMELHO = 10, // 10
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+}
+
+enum Cores {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA,
+}
+
+export function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+console.log(1);
+
+escolhaACor(Cores.ROSA);
